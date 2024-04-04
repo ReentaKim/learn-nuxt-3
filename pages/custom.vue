b6727b64444ff5ae7745981cd1cbd6914e0a5b5c
ReentaKim <reenta.kim@gmail.com>
2024-04-04 22:08:49 +0900

    레이아웃 구성 - 기초부터 실전까지

---
+++ pages/custom.vue
@@ -0,0 +1,19 @@
+<template>
+  <!-- <div>
+    custom
+    <NuxtPage />
+  </div> -->
+  <div>
+    <NuxtLayout name="custom">
+      <NuxtPage />
+    </NuxtLayout>
+  </div>
+</template>
+
+<script setup lang="ts">
+defindPageMeta({
+  layout: false,
+});
+</script>
+
+<style scoped></style>
